cfe56d53d2d8c7190c955f9f0e9b241407666df4
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-12-15 12:18:25 -0600

    added stats repo to display stats on html

--- src/main/java/masterofmusic/masterofmusic/Controllers/ProfileController.java
+++ src/main/java/masterofmusic/masterofmusic/Controllers/ProfileController.java
@@ -1,5 +1,7 @@
 package masterofmusic.masterofmusic.Controllers;
 
+import masterofmusic.masterofmusic.repositories.StatsRepository;
+import masterofmusic.masterofmusic.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +10,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class ProfileController {
 
-//    private final /*someRepo*/ profileDao
+    private final UserRepository profileDao;
+    private final StatsRepository statsDao;
+
+    public ProfileController(UserRepository profileDao, StatsRepository statsDao){
+        this.profileDao = profileDao;
+        this.statsDao = statsDao;
+    }
 
     @GetMapping("/profile/{id}")
     public String viewProfile(@PathVariable long id, Model model){
-        model.addAttribute("profile");
+        model.addAttribute("profile", profileDao.getOne(id));
+        model.addAttribute("stats", statsDao.findAllByUserId(id));
         return "/profile";
     }
+
+
 
 }
