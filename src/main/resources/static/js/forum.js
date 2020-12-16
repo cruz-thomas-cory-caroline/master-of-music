855d0ab4d0c00ba1140833617121860d5fdbadfa
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-12-16 10:21:16 -0600

    feat: only sign in users can post new comments, reply windows now open and can be close

--- src/main/resources/static/js/forum.js
+++ src/main/resources/static/js/forum.js
@@ -6,9 +6,27 @@
         $('#new-post').show()
         $('#post-form').hide()
     })
+
+    $('.post-reply').hide()
+
     $('#close-post').click(function () {
         $('#new-post').hide()
         $('#post-form').show()
+    })
+
+    let buttonValue;
+
+    $('.reply').click(function () {
+        $('.post-reply').hide()
+        buttonValue = $(this).attr("value");
+        $('#'+buttonValue).show()
+        console.log(buttonValue)
+    })
+
+    $('.close-reply').click(function () {
+        buttonValue = $(this).attr("value");
+        $('#'+buttonValue).hide()
+        console.log(buttonValue)
     })
 
 })();
