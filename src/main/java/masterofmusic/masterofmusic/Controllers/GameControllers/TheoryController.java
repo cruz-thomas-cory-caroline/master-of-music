16c7b52640bc1eb2ef10f0627dab5d1a242314cf
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-12-16 12:00:09 -0600

    display answers to their corresponding questions

--- src/main/java/masterofmusic/masterofmusic/Controllers/GameControllers/TheoryController.java
+++ src/main/java/masterofmusic/masterofmusic/Controllers/GameControllers/TheoryController.java
@@ -2,6 +2,7 @@ package masterofmusic.masterofmusic.Controllers.GameControllers;
 
 
 import masterofmusic.masterofmusic.models.Question;
+import masterofmusic.masterofmusic.repositories.AnswerRepository;
 import masterofmusic.masterofmusic.repositories.QuestionRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,15 +17,19 @@ import java.util.List;
 public class TheoryController {
 
     private final QuestionRepository questionDao;
+    private final AnswerRepository answerDao;
 
-    public TheoryController(QuestionRepository questionDao){
+    public TheoryController(QuestionRepository questionDao, AnswerRepository answerDao){
         this.questionDao = questionDao;
+        this.answerDao = answerDao;
     }
 
     @GetMapping("/music-theory/{id}")
     public String viewQuizFormat(@PathVariable int id, Model model){
         ArrayList<Question> theoryList = questionDao.findAllByGameId(2L);
         model.addAttribute("questions", theoryList.get(id).getQuestion());
+        long questionId = theoryList.get(id).getId();
+        model.addAttribute("answers", answerDao.getAllByQuestionId(questionId));
         return "/music-theory";
     }
 
---
+++ src/main/java/masterofmusic/masterofmusic/repositories/AnswerRepository.java
@@ -0,0 +1,14 @@
+package masterofmusic.masterofmusic.repositories;
+
+
+import masterofmusic.masterofmusic.models.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    List<Answer> getAllByQuestionId(long id);
+}
