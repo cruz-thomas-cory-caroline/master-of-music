027d16d96bf5a528e1131610fbc323e397ac8e2d
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-12-17 13:47:22 -0600

    able to pull user answers from view

--- src/main/java/masterofmusic/masterofmusic/Controllers/GameControllers/TheoryController.java
+++ src/main/java/masterofmusic/masterofmusic/Controllers/GameControllers/TheoryController.java
@@ -1,13 +1,13 @@
 package masterofmusic.masterofmusic.controllers.GameControllers;
 
 
+import masterofmusic.masterofmusic.models.Answer;
 import masterofmusic.masterofmusic.models.Question;
 import masterofmusic.masterofmusic.repositories.AnswerRepository;
 import masterofmusic.masterofmusic.repositories.QuestionRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +31,24 @@ public class TheoryController {
         long questionId = theoryList.get(id).getId();
         model.addAttribute("answers", answerDao.getAllByQuestionId(questionId));
         return "/music-theory";
+    }
+
+
+    @PostMapping("/music-theory/{id}")
+    public String createScore(@RequestParam(name = "answers")String userAnswer, @PathVariable int id){
+        ArrayList<Question> theoryList = questionDao.findAllByGameId(2L);
+        long questionId = theoryList.get(id).getId();
+        ArrayList<Answer> answerList = answerDao.getAllByQuestionId(questionId);
+        String correctAnswer;
+        for (Answer answer: answerList){
+            if(answer.isCorrect()){
+                correctAnswer = answer.getAnswer();
+            }
+        }
+        System.out.println(userAnswer);
+//        System.out.println(correctAnswer);
+
+        return "music-theory";
     }
 
 }
