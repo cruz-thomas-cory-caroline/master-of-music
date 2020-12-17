9d94f6133b2f6dbb8cc86edcf0d2512ba932e2f5
Caroline E <caroline.esquivel01@gmail.com>
2020-12-17 15:32:37 -0600

    .

--- src/main/java/masterofmusic/masterofmusic/Controllers/GameControllers/LyricsController.java
+++ src/main/java/masterofmusic/masterofmusic/Controllers/GameControllers/LyricsController.java
@@ -1,31 +1,53 @@
 package masterofmusic.masterofmusic.controllers.GameControllers;
 
+import masterofmusic.masterofmusic.models.Answer;
 import masterofmusic.masterofmusic.models.Question;
+import masterofmusic.masterofmusic.models.Song;
 import masterofmusic.masterofmusic.repositories.AnswerRepository;
 import masterofmusic.masterofmusic.repositories.QuestionRepository;
+import masterofmusic.masterofmusic.repositories.SongRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class LyricsController {
-    private final QuestionRepository questionDao;
+    private final SongRepository songDao;
     private final AnswerRepository answerDao;
 
-    public LyricsController(QuestionRepository questionDao, AnswerRepository answerDao) {
-        this.questionDao = questionDao;
+    public LyricsController(SongRepository songDao, AnswerRepository answerDao) {
+        this.songDao = songDao;
         this.answerDao = answerDao;
     }
 
-    @GetMapping("/finish-lyrics/{id}")
-    public String viewQuizFormat(@PathVariable int id, Model model){
-        ArrayList<Question> lyrics = questionDao.findAllByGameId(1L);
-        model.addAttribute("questions", lyrics.get(id).getQuestion());
-        long questionId = lyrics.get(id).getId();
-        model.addAttribute("answers", answerDao.getAllByQuestionId(questionId));
+    @GetMapping("/finish-lyrics")
+    public String viewQuizFormat( Model model) {
+        model.addAttribute("songs", songDao.findAll());
+        int count = songDao.findAll().size();
+        List<List<String>> lyricAnswers = new ArrayList<>();
+        List<Long> chosenSongs = new ArrayList<>();
+
+        for (Song song : songDao.findAll()) {
+            int counter = 4;
+            List<String> lyricsSet = new ArrayList<>();
+            while (counter > 0) {
+                Song songOne = songDao.getOne(((long) (Math.random() * ((count + 1) - 1 + 1) + 1)));
+                for (Long songId : chosenSongs) {
+                    if (songId != songOne.getId()) {
+                        chosenSongs.add(songOne.getId());
+                        lyricsSet.add(songOne.getLyrics());
+                        break;
+                    }
+                }
+                counter = counter - 1;
+            }
+            lyricAnswers.add(lyricsSet);
+        }
+        model.addAttribute("answers", lyricAnswers);
         return "finish-lyrics";
     }
 }
