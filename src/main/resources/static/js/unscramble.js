8ec80c303429c9aa6886e48f74047c06998d3679
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-12-19 12:17:08 -0600

    feat: dragged items now stick to center of drop zone

--- src/main/resources/static/js/unscramble.js
+++ src/main/resources/static/js/unscramble.js
@@ -1,21 +1,30 @@
 (function () {
     "use strict";
 
-    $(".words").draggable()
+    $(".words").draggable({
+        opacity: .4,
+    })
 
     $('.drop-zone').droppable({
         accept: ".words",
         drop: function (event, ui) {
             $(".words").draggable()
             console.log(ui.draggable)
-            $(this).find("input").val()
-            console.log($(ui).val())
-            console.log($(this).find("input").attr("innerHTML"))
+            $(this).find("input").val(ui.draggable[0].innerHTML)
+            console.log($(this).find("input").val())
+            snapToMiddle(ui.draggable,$(this));
             //     ui.draggable.remove();
             //     $(this).append(itemDisplay("cement_build"))
             // }
         }
     })
+
+    function snapToMiddle(dragger, target){
+        var offset = target.offset();
+        var topMove = (target.outerHeight(true) - dragger.outerHeight(true)) / 2;
+        var leftMove= (target.outerWidth(true) - dragger.outerWidth(true)) / 2;
+        dragger.offset({ top: topMove + offset.top, left: leftMove + offset.left })
+    }
 
 
 })();
