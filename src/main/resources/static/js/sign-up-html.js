0095496ce34c007ef5265ace115efa0b6859a9a9
Caroline E <caroline.esquivel01@gmail.com>
2020-12-19 23:51:56 -0600

    restyling pw strength bar

---
+++ src/main/resources/static/js/sign-up-html.js
@@ -0,0 +1,12 @@
+(function () {
+    "use strict";
+
+    $("#pwRequirementsList").hide();
+
+$(document).ready(function() {
+    $("#pwRequirements").click(function () {
+        $("ul").show();
+    });
+});
+
+})();
