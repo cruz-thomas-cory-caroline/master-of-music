64b0ca19660d76eb23486101ee5912f6215018ac
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-12-20 15:15:09 -0600

    feat: draggables now revert to original position, border style changes when drop zones are occupied

--- src/main/resources/static/js/unscramble.js
+++ src/main/resources/static/js/unscramble.js
@@ -1,8 +1,18 @@
 (function () {
     "use strict";
 
+    $(".all-cards").hide();
+    $(".all-cards").first().show()
+
     $(".words").draggable({
         opacity: .4,
+        revert:  function(event, ui) {
+            $(this).data("uiDraggable").originalPosition = {
+                top : 0,
+                left : 0
+            };
+            return !event;
+        }
     })
 
     $('.drop-zone').droppable({
@@ -10,21 +20,31 @@
         drop: function (event, ui) {
             $(".words").draggable()
             console.log(ui.draggable)
+            $(this).css('border', 'none')
             $(this).find("input").val(ui.draggable[0].innerHTML)
             console.log($(this).find("input").val())
-            snapToMiddle(ui.draggable,$(this));
+            snapToMiddle(ui.draggable, $(this))
             //     ui.draggable.remove();
             //     $(this).append(itemDisplay("cement_build"))
             // }
+        },
+        out: function() {
+            $(this).css({'border': 'solid black 1px', 'width': '75px', 'height': '50px'})
         }
     })
 
-    function snapToMiddle(dragger, target){
+
+    function snapToMiddle(dragger, target) {
         var offset = target.offset();
         var topMove = (target.outerHeight(true) - dragger.outerHeight(true)) / 2;
-        var leftMove= (target.outerWidth(true) - dragger.outerWidth(true)) / 2;
-        dragger.offset({ top: topMove + offset.top, left: leftMove + offset.left })
+        var leftMove = (target.outerWidth(true) - dragger.outerWidth(true)) / 2;
+        dragger.offset({top: topMove + offset.top, left: leftMove + offset.left})
     }
+
+    $(".cat-button").click(function () {
+        $(this).parent().parent().hide()
+        $(this).parent().parent().parent().next().first().show()
+    })
 
 
 })();
