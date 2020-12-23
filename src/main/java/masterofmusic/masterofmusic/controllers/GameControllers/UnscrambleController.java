23e455d908561d51c6b5a1b01eae8c0e18a351dd
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-12-23 13:52:53 -0600

    feat: backend now receives user answers and scores accordingly

--- src/main/java/masterofmusic/masterofmusic/controllers/GameControllers/UnscrambleController.java
+++ src/main/java/masterofmusic/masterofmusic/controllers/GameControllers/UnscrambleController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.concurrent.ThreadLocalRandom;
 
 import java.lang.reflect.Array;
@@ -20,85 +21,100 @@ import java.util.regex.Pattern;
 public class UnscrambleController {
     private final SongRepository songDao;
     private final GameRepository gameDao;
+    private List<Integer> chosenSongIDs = new ArrayList<>();
+    private List<Song> chosenSongs = new ArrayList<>();
 
     public UnscrambleController(SongRepository songDao, GameRepository gameDao) {
         this.songDao = songDao;
         this.gameDao = gameDao;
     }
 
-    @PostMapping("/unscramble/start")
-    public String questionsToGet(@RequestParam(name = "count") int numberOfQuestions,
-                                 @RequestParam(name = "inlineRadioOptions") String difficulty,
-                                 @RequestParam(name = "inlineRadioOptions1") String genre) {
-        System.out.println(numberOfQuestions);
-        System.out.println(difficulty);
-        System.out.println(genre);
-        return "posts/index";
+//    @RequestParam(name = "count") int numberOfQuestions,
+//    @RequestParam(name = "inlineRadioOptions") String difficulty,
+//    @RequestParam(name = "inlineRadioOptions1") String genre
+
+    @GetMapping("/unscramble")
+    public String unscrambleGame(Model model) {
+
+        chosenSongs = songDao.findAll();
+
+        List<Song> songs = songDao.findAll();
+        List<String> lyricsToScramble = new ArrayList<>();
+        List<String> lyricsStart = new ArrayList<>();
+        List<List<String>> scrambledLyricsList = new ArrayList<>();
+
+        for (Song song : songs) {
+            lyricsToScramble.add(song.getLyrics());
+        }
+
+        for (String lyric : lyricsToScramble) {
+            Pattern pattern = Pattern.compile("\\w+");
+            Matcher matcher = pattern.matcher(lyric);
+            List<String> singleWords = new ArrayList<>();
+            while(matcher.find()) {
+                singleWords.add(matcher.group());
+            }
+            String str[] = lyric.split(" ");
+            singleWords = Arrays.asList(str);
+
+            int lyricLength = singleWords.size();
+            System.out.println(lyricLength);
+            for (String wordshow : singleWords) {
+                System.out.println(wordshow + " " + singleWords.indexOf(wordshow));
+            }
+
+            List<String> scrambledLyric = new ArrayList<>();
+            String lyricStart = "";
+            String lyricEnd = "";
+            int wordCount = (int) (Math.random() * ((lyricLength - 1) + 1) + 1);
+            int switchChoice = (int) (Math.random() * ((4 - 1) + 1) + 1);
+
+            switch (1) {
+                case 1:
+                    chosenSongIDs = new ArrayList<>();
+                    List<Integer> indexesChosen = new ArrayList<>();
+                    do {
+                        int indexToAdd = ThreadLocalRandom.current().nextInt(0, lyricLength);
+                        if (!chosenSongIDs.contains(indexToAdd)) {
+                            chosenSongIDs.add(indexToAdd);
+                            scrambledLyric.add(singleWords.get(indexToAdd));
+                        }
+                    } while (scrambledLyric.size() < singleWords.size());
+
+//                    THIS IS FOR SHOWING THE START OF THE LYRIC UNSCRAMBLED AND THE END SCRAMBLED
+//                    for (var i = 0; i < wordCount; i++) {
+//                        lyricStart += singleWords.get(i);
+//                        lyricStartToCombine.add(singleWords.get(i));
+//                    }
+//                    for (var i = wordCount; i < lyricLength; i++) {
+//                        lyricEnd += singleWords.get(i);
+//                        lyricStartToCombine.add(singleWords.get(i));
+//                    }
+                    break;
+                default:
+                    break;
+            }
+            scrambledLyricsList.add(scrambledLyric);
+        }
+        model.addAttribute("scrambledLyricsSet", scrambledLyricsList);
+        model.addAttribute("originalLyrics", lyricsToScramble);
+        model.addAttribute("songs", songs);
+        return "unscramble";
     }
 
-//    @GetMapping("/unscramble")
-//    public String unscrambleGame(Model model) {
-//        List<Song> songs = songDao.findAll();
-//        List<String> lyricsToScramble = new ArrayList<>();
-//        List<String> lyricsStart = new ArrayList<>();
-//        List<List<String>> scrambledLyricsList = new ArrayList<>();
-//
-//        for (Song song : songs) {
-//            lyricsToScramble.add(song.getLyrics());
-//        }
-//
-//        for (String lyric : lyricsToScramble) {
-//            Pattern pattern = Pattern.compile("\\w+");
-//            Matcher matcher = pattern.matcher(lyric);
-//            List<String> singleWords = new ArrayList<>();
-//            while(matcher.find()) {
-//                singleWords.add(matcher.group());
-//            }
-//            String str[] = lyric.split(" ");
-//            List<String> singleWords;
-//            singleWords = Arrays.asList(str);
-//
-//            int lyricLength = singleWords.size();
-//            System.out.println(lyricLength);
-//            for (String wordshow : singleWords) {
-//                System.out.println(wordshow + " " + singleWords.indexOf(wordshow));
-//            }
-//
-//            List<String> scrambledLyric = new ArrayList<>();
-//            String lyricStart = "";
-//            String lyricEnd = "";
-//            int wordCount = (int) (Math.random() * ((lyricLength - 1) + 1) + 1);
-//            int switchChoice = (int) (Math.random() * ((4 - 1) + 1) + 1);
-//
-//            switch (1) {
-//                case 1:
-//                    List<Integer> indexesChosen = new ArrayList<>();
-//                    do {
-//                        int indexToAdd = ThreadLocalRandom.current().nextInt(0, lyricLength);
-//                        if (!indexesChosen.contains(indexToAdd)) {
-//                            indexesChosen.add(indexToAdd);
-//                            scrambledLyric.add(singleWords.get(indexToAdd));
-//                        }
-//                    } while (scrambledLyric.size() < singleWords.size());
-//
-////                    THIS IS FOR SHOWING THE START OF THE LYRIC UNSCRAMBLED AND THE END SCRAMBLED
-////                    for (var i = 0; i < wordCount; i++) {
-////                        lyricStart += singleWords.get(i);
-////                        lyricStartToCombine.add(singleWords.get(i));
-////                    }
-////                    for (var i = wordCount; i < lyricLength; i++) {
-////                        lyricEnd += singleWords.get(i);
-////                        lyricStartToCombine.add(singleWords.get(i));
-////                    }
-//                    break;
-//                default:
-//                    break;
-//            }
-//            scrambledLyricsList.add(scrambledLyric);
-//        }
-//        model.addAttribute("scrambledLyricsSet", scrambledLyricsList);
-//        model.addAttribute("originalLyrics", lyricsToScramble);
-//        model.addAttribute("songs", songs);
-//        return "unscramble";
-//    }
+
+    @PostMapping("/unscramble/results")
+    public String submitAnswers(HttpServletRequest request, Model model) {
+        int score = 0;
+        for(Song song : chosenSongs) {
+            System.out.println(request.getParameter("song"+chosenSongs.indexOf(song)));
+            System.out.println(song.getLyrics());
+            if (request.getParameter("song"+chosenSongs.indexOf(song)).equalsIgnoreCase(song.getLyrics())) {
+                score++;
+            }
+        }
+        model.addAttribute("score", score);
+        return "final";
+    }
+
 }
