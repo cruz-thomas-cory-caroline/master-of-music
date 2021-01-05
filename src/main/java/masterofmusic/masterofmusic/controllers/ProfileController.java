ca2c6e7a8158e458b504894fb960ace54c6243a3
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2021-01-05 09:34:35 -0600

    overall score is updated after every correctly answered question

--- src/main/java/masterofmusic/masterofmusic/controllers/ProfileController.java
+++ src/main/java/masterofmusic/masterofmusic/controllers/ProfileController.java
@@ -1,11 +1,16 @@
 package masterofmusic.masterofmusic.controllers;
 
+import masterofmusic.masterofmusic.models.PlayerGame;
+import masterofmusic.masterofmusic.models.User;
 import masterofmusic.masterofmusic.repositories.StatsRepository;
 import masterofmusic.masterofmusic.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
 
 @Controller
 public class ProfileController {
@@ -20,8 +25,18 @@ public class ProfileController {
 
     @GetMapping("/profile/{id}")
     public String viewProfile(@PathVariable long id, Model model){
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         model.addAttribute("profile", profileDao.getOne(id));
-        model.addAttribute("stats", statsDao.findAllByUserId(id));
+
+        ArrayList<PlayerGame> userGames = statsDao.findAllByUserId(user.getId());
+        long counter = 0;
+        for (PlayerGame game: userGames) {
+            long score = game.getScore();
+            counter += score;
+        }
+        System.out.println(counter);
+
+        model.addAttribute("stats", counter);
         return "/profile";
     }
 
