68da13248628a8d880eeac7a6b5ba4e73e73b8c6
Cory <courtneynholley@gmail.com>
2021-01-06 11:22:29 -0600

    Feat: avatar selection and saving to database working, split main js and trivia js

--- src/main/java/masterofmusic/masterofmusic/controllers/ProfileController.java
+++ src/main/java/masterofmusic/masterofmusic/controllers/ProfileController.java
@@ -3,11 +3,13 @@ package masterofmusic.masterofmusic.controllers;
 import masterofmusic.masterofmusic.models.User;
 import masterofmusic.masterofmusic.repositories.StatsRepository;
 import masterofmusic.masterofmusic.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProfileController {
@@ -20,17 +22,26 @@ public class ProfileController {
         this.statsDao = statsDao;
     }
 
-    @GetMapping("/profile/{id}")
-    public String viewProfile(@PathVariable long id, Model model){
-        model.addAttribute("user", userDao.getOne(id));
+    @GetMapping("/profile")
+    public String viewProfile(
+            Model model
+    ){
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("user", userDao.getOne(user.getId()));
         return "profile";
     }
 
     @PostMapping("/profile/avatar/{id}")
-    public String changeAvatar(@PathVariable long id) {
-        id = 3;
+    public String changeAvatar(
+            @PathVariable long id,
+            @RequestParam(name = "avatarSelection") String avatarSelected,
+            Model model
+    ) {
         User user = userDao.findById(id);
-
+        user.setImages(avatarSelected);
+        userDao.save(user);
+        System.out.println(avatarSelected);
+        model.addAttribute("user", user);
         return "profile";
     }
 
