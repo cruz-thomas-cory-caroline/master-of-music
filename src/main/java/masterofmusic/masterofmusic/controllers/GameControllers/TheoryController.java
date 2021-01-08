d596f6b01e50774e407f419b9a246a2b1599f31a
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2021-01-08 09:51:13 -0600

    commiting before pulling from staging

--- src/main/java/masterofmusic/masterofmusic/controllers/GameControllers/TheoryController.java
+++ src/main/java/masterofmusic/masterofmusic/controllers/GameControllers/TheoryController.java
@@ -1,4 +1,4 @@
-package masterofmusic.masterofmusic.Controllers.GameControllers;
+package masterofmusic.masterofmusic.controllers.GameControllers;
 
 import masterofmusic.masterofmusic.models.*;
 
@@ -113,14 +113,17 @@ public class TheoryController {
             correctAnswer(10,"easy");
             System.out.println(difficultySelection);
             model.addAttribute("correct", "Great Job!");
+            model.addAttribute("score",10);
         }else if(userAnswer.equalsIgnoreCase(correctAnswer) && difficultySelection.equalsIgnoreCase("option2")){
             correctAnswer(45,"medium");
             System.out.println(difficultySelection);
             model.addAttribute("correct", "Great Job!");
+            model.addAttribute("score",45);
         }else if(userAnswer.equalsIgnoreCase(correctAnswer) && difficultySelection.equalsIgnoreCase("option3")){
                 correctAnswer(100,"hard");
                 System.out.println(difficultySelection);
                 model.addAttribute("correct", "Great Job!");
+            model.addAttribute("score",100);
         }
 
         model.addAttribute("songDifficulty", difficultySelection);
