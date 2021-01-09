b6499125e1a79db7901655698ed68076675dcf6f
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2021-01-09 16:40:58 -0600

    feat: reworked achievement page, all achievements show as guest, when logged in show if earned, grey if not

--- src/main/java/masterofmusic/masterofmusic/controllers/AchievementsController.java
+++ src/main/java/masterofmusic/masterofmusic/controllers/AchievementsController.java
@@ -33,66 +33,28 @@ public class AchievementsController {
         this.achievementDao = achievementDao;
     }
 
-    @GetMapping("/achievement/{id}")
-    public String showBadges(
-            @PathVariable long id,
-            Model model
-    ) {
-        User user = userDao.findById(3L);
-
-        ArrayList<PlayerGame> playerGamesForUser = playerGameDao.findAllByUserId(user.getId());
-        ArrayList<List<PlayerGameRound>> playerGameRoundsForTrivia = new ArrayList<>();
-        boolean triviaGemAward;
-        boolean easyPerfect = false;
-        boolean mediumPerfect = false;
-        boolean hardPerfect = false;
-
-        for (PlayerGame playerGame : playerGamesForUser) {
-            if (playerGame.getGame().getId() == 3) {
-                playerGameRoundsForTrivia.add(playerGame.getPlayerGameRounds());
-            }
-        }
-
-        for (List<PlayerGameRound> playerGameRound : playerGameRoundsForTrivia) {
-            for (PlayerGameRound playerGameRound1 : playerGameRound) {
-                if (playerGameRound1.getDifficulty().equals("easy") && playerGameRound1.getScore() == 500) {
-                    easyPerfect = true;
-                    if (playerGameRound1.getDifficulty().equals("medium") && playerGameRound1.getScore() == 500) {
-                        mediumPerfect = true;
-                        if (playerGameRound1.getDifficulty().equals("hard") && playerGameRound1.getScore() == 500) {
-                            hardPerfect = true;
-                        }
-                    }
-                }
-            }
-        }
-
-        System.out.println(easyPerfect);
-        System.out.println(mediumPerfect);
-        System.out.println(hardPerfect);
-
-        return "achievements";
-    }
-
     @GetMapping("/achievements")
     public String showAchievements(Model model) {
 
         if (!SecurityContextHolder.getContext().getAuthentication().getName().equalsIgnoreCase("anonymousUser")) {
             User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-            List<Achievement> userAchievements = user.getUsers_achievements();
+            User thisUser = userDao.getOne(user.getId());
+            List<Achievement> userAchievements = thisUser.getUsers_achievements();
             if (userAchievements == null) {
+                System.out.println("I'm EMpty");
                 userAchievements = new ArrayList<>();
             }
             model.addAttribute("loggedIn", true);
             model.addAttribute("userAchievements", userAchievements);
+
+            for (Achievement ach : userAchievements) {
+                System.out.println(ach.getName());
+            }
         }
 
         List<Achievement> allAchievements = achievementDao.findAll();
         model.addAttribute("allAchievements", allAchievements);
-
-        return "achievementsV2";
-
-
+        return "achievements";
     }
 
 
