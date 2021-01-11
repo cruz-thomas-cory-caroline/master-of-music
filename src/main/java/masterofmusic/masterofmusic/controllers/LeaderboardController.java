52c5e3082b59fb59de843dcd0e9a8aa848e55372
Cory <courtneynholley@gmail.com>
2021-01-11 14:41:24 -0600

    Feat: Styling on leaderboard, added badges to global user rankings

--- src/main/java/masterofmusic/masterofmusic/controllers/LeaderboardController.java
+++ src/main/java/masterofmusic/masterofmusic/controllers/LeaderboardController.java
@@ -30,28 +30,32 @@ public class LeaderboardController {
 
         boolean loggedIn = false;
 
-       if (!SecurityContextHolder.getContext().getAuthentication().getName().equalsIgnoreCase("anonymousUser")) {
-           loggedIn = true;
-       }
+        if (!SecurityContextHolder.getContext().getAuthentication().getName().equalsIgnoreCase("anonymousUser")) {
+            loggedIn = true;
+        }
 
         List<User> rankedUserList = userDao.getRankedListOfUsersByOverallScore();
         List<Integer> rankedScores = userDao.getRankedListOfAllOverallScores();
 
         for (Game game : gameDao.findAll()) {
             List<User> rankedUsersByGame = userDao.getRankedListOfUsersByGameId(game.getId());
-            model.addAttribute("game"+game.getId()+"Players", rankedUsersByGame);
-            model.addAttribute("game"+game.getId()+"Scores", userDao.getRankedListOfUsersScoreByGameId(game.getId()));
+            model.addAttribute("game" + game.getId() + "Players", rankedUsersByGame);
+            model.addAttribute("game" + game.getId() + "Scores", userDao.getRankedListOfUsersScoreByGameId(game.getId()));
 
             if (loggedIn) {
                 User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
                 User userToFind = userDao.getOne(user.getId());
-                model.addAttribute("game"+game.getId()+"Standing", rankedUsersByGame.indexOf(userToFind)+1);
-                System.out.println(rankedUserList.indexOf(user)+1);
-                model.addAttribute("globalRank", (rankedUserList.indexOf(userToFind))+1);
-                model.addAttribute("playerScore", rankedScores.get(rankedUserList.indexOf(userToFind)));
+                model.addAttribute("game" + game.getId() + "Standing", rankedUsersByGame.indexOf(userToFind) + 1);
+                System.out.println(rankedUserList.indexOf(user) + 1);
+                if (rankedUserList.contains(userToFind)) {
+                    model.addAttribute("globalRank", (rankedUserList.indexOf(userToFind)) + 1);
+                    model.addAttribute("playerScore", rankedScores.get(rankedUserList.indexOf(userToFind)));
+                }
                 model.addAttribute("loggedIn", true);
+                model.addAttribute("userToFind", userToFind);
             }
         }
+
 
         model.addAttribute("rankedUsers", rankedUserList);
         model.addAttribute("rankedScores", rankedScores);
