260defd2d99b0d844286dcee7097ac35d3252064
Caroline E <caroline.esquivel01@gmail.com>
2021-01-12 14:08:48 -0600

    url

--- src/main/java/masterofmusic/masterofmusic/controllers/TestsController.java
+++ src/main/java/masterofmusic/masterofmusic/controllers/TestsController.java
@@ -1,9 +1,10 @@
 package masterofmusic.masterofmusic.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+@Controller
 public class TestsController {
 
     @Value("${app.url}")
