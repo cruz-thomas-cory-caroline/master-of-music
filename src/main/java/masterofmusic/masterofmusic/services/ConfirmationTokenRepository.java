c46f734a4bf1c1d45a8d6b3db2c18814635ed877
Caroline E <caroline.esquivel01@gmail.com>
2021-01-12 23:13:28 -0600

    confirmation repo

---
+++ src/main/java/masterofmusic/masterofmusic/services/ConfirmationTokenRepository.java
@@ -0,0 +1,9 @@
+package masterofmusic.masterofmusic.services;
+
+
+import masterofmusic.masterofmusic.models.ConfirmationToken;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ConfirmationTokenRepository extends CrudRepository<ConfirmationToken, String> {
+    ConfirmationToken findByConfirmationToken(String confirmationToken);
+}
